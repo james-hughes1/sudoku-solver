c2c633d19d9cb205009bcefdf7b8c9a7c44cf75a
James Hughes <jh2284@cam.ac.uk>
2023-12-04 11:16:12 +0000

    Change: In find_valid_templates, check if template is in same row, column, box as each digit checked

--- src/sudokutools/solve.py
+++ src/sudokutools/solve.py
@@ -21,16 +21,30 @@ def generate_templates():
 
 
 def find_valid_templates(grid):
-    valid_templates_list = [[]] * 9
+    BOX_GRIDS = np.zeros((9, 9, 9))
+    for grid_idx in range(9):
+        BOX_GRIDS[
+            grid_idx,
+            3 * (grid_idx // 3) : 3 * (grid_idx // 3) + 3,
+            3 * (grid_idx % 3) : 3 * (grid_idx % 3) + 3,
+        ] = 1
+    valid_templates_list = [[], [], [], [], [], [], [], [], []]
     for template_idx, template in enumerate(generate_templates()):
         for digit in range(1, 10):
             valid = True
             for i in range(9):
                 for j in range(9):
-                    other_digits = list(range(9))
+                    other_digits = list(range(1, 10))
                     other_digits.remove(digit)
                     if grid[i][j] in other_digits and template[i][j] == 1:
                         valid = False
+            digit_grid = (grid == digit) + (template == 1)
+            if (
+                (np.sum(digit_grid, axis=0) > 1).any()
+                or (np.sum(digit_grid, axis=1) > 1).any()
+                or (np.sum(BOX_GRIDS * digit_grid, axis=(1, 2)) > 1).any()
+            ):
+                valid = False
             if valid:
                 valid_templates_list[digit - 1].append(template_idx)
     return valid_templates_list
