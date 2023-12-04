efd69c43b0c8d6d61bc93b33f8fe73a254a97027
James Hughes <jh2284@cam.ac.uk>
2023-12-04 19:08:46 +0000

    Refactor: Remove duplicate code across tests in test_solve

--- test/test_solve.py
+++ test/test_solve.py
@@ -7,6 +7,35 @@ from src.sudokutools.solve import (
 )
 
 
+start_grid_easy = np.array(
+    [
+        [1, 2, 3, 4, 5, 6, 7, 8, 0],
+        [4, 5, 6, 7, 0, 9, 1, 2, 3],
+        [0, 8, 9, 1, 2, 3, 4, 5, 6],
+        [2, 3, 4, 5, 0, 7, 8, 9, 1],
+        [0, 6, 7, 8, 9, 1, 2, 3, 4],
+        [8, 9, 1, 2, 3, 0, 5, 6, 7],
+        [0, 4, 5, 6, 7, 8, 9, 1, 2],
+        [6, 7, 8, 9, 1, 0, 3, 4, 5],
+        [9, 0, 2, 3, 4, 5, 6, 7, 8],
+    ]
+)
+
+solution_grid_easy_expected = np.array(
+    [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [4, 5, 6, 7, 8, 9, 1, 2, 3],
+        [7, 8, 9, 1, 2, 3, 4, 5, 6],
+        [2, 3, 4, 5, 6, 7, 8, 9, 1],
+        [5, 6, 7, 8, 9, 1, 2, 3, 4],
+        [8, 9, 1, 2, 3, 4, 5, 6, 7],
+        [3, 4, 5, 6, 7, 8, 9, 1, 2],
+        [6, 7, 8, 9, 1, 2, 3, 4, 5],
+        [9, 1, 2, 3, 4, 5, 6, 7, 8],
+    ]
+)
+
+
 def test_templates():
     count_array = np.zeros((9, 9))
     for template_array in generate_templates():
@@ -16,50 +45,11 @@ def test_templates():
 
 
 def test_valid_templates():
-    start_grid = np.array(
-        [
-            [1, 2, 3, 4, 5, 6, 7, 8, 0],
-            [4, 5, 6, 7, 0, 9, 1, 2, 3],
-            [0, 8, 9, 1, 2, 3, 4, 5, 6],
-            [2, 3, 4, 5, 0, 7, 8, 9, 1],
-            [0, 6, 7, 8, 9, 1, 2, 3, 4],
-            [8, 9, 1, 2, 3, 0, 5, 6, 7],
-            [0, 4, 5, 6, 7, 8, 9, 1, 2],
-            [6, 7, 8, 9, 1, 0, 3, 4, 5],
-            [9, 0, 2, 3, 4, 5, 6, 7, 8],
-        ]
-    )
-    valid_templates_list = find_valid_templates(start_grid)
+    valid_templates_list = find_valid_templates(start_grid_easy)
     for template_idx_list in valid_templates_list:
         assert len(template_idx_list) == 1
 
 
 def test_solve_backtrack_valid():
-    start_grid = np.array(
-        [
-            [1, 2, 3, 4, 5, 6, 7, 8, 0],
-            [4, 5, 6, 7, 8, 9, 1, 0, 3],
-            [7, 8, 9, 1, 2, 3, 0, 5, 6],
-            [2, 3, 4, 5, 6, 0, 8, 9, 1],
-            [5, 6, 7, 8, 0, 1, 2, 3, 4],
-            [8, 9, 1, 0, 3, 4, 5, 6, 7],
-            [3, 4, 0, 6, 7, 8, 9, 1, 2],
-            [6, 0, 8, 9, 1, 2, 3, 4, 5],
-            [0, 1, 2, 3, 4, 5, 6, 7, 8],
-        ]
-    )
-    solution_grid = solve_backtrack(start_grid)
-    solution_grid_expected = np.array(
-        [
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [4, 5, 6, 7, 8, 9, 1, 2, 3],
-            [7, 8, 9, 1, 2, 3, 4, 5, 6],
-            [2, 3, 4, 5, 6, 7, 8, 9, 1],
-            [5, 6, 7, 8, 9, 1, 2, 3, 4],
-            [8, 9, 1, 2, 3, 4, 5, 6, 7],
-            [3, 4, 5, 6, 7, 8, 9, 1, 2],
-            [6, 7, 8, 9, 1, 2, 3, 4, 5],
-            [9, 1, 2, 3, 4, 5, 6, 7, 8],
-        ]
-    )
-    assert (solution_grid == solution_grid_expected).all()
+    solution_grid = solve_backtrack(start_grid_easy)
+    assert (solution_grid == solution_grid_easy_expected).all()
