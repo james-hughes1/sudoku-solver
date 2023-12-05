0e6917d6572b61f851ad66f65262e30e18807220
James Hughes <jh2284@cam.ac.uk>
2023-12-05 22:37:31 +0000

    Feat: Create read_grid, passing the most basic tests but not catching errors

--- src/sudokutools/data.py
+++ src/sudokutools/data.py
@@ -1,5 +1,16 @@
+import numpy as np
+
+
 def read_grid(filepath):
-    return 0
+    with open(filepath, "r") as file:
+        file_lines = file.readlines()
+    file_digits = []
+    for line in file_lines:
+        line_digits = [int(char) for char in line if char.isdigit()]
+        if line_digits:
+            file_digits.append(line_digits)
+    grid = np.array(file_digits)
+    return grid
 
 
 def write_grid(grid, filename=None):
