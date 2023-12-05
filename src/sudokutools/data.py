befb2fc47efda50bed60d343ad0201db31422b68
James Hughes <jh2284@cam.ac.uk>
2023-12-05 23:27:08 +0000

    Change: Change read_grid, passes error catching tests

--- src/sudokutools/data.py
+++ src/sudokutools/data.py
@@ -2,15 +2,40 @@ import numpy as np
 
 
 def read_grid(filepath):
-    with open(filepath, "r") as file:
-        file_lines = file.readlines()
-    file_digits = []
-    for line in file_lines:
-        line_digits = [int(char) for char in line if char.isdigit()]
-        if line_digits:
-            file_digits.append(line_digits)
-    grid = np.array(file_digits)
-    return grid
+    if filepath[-4:] != ".txt":
+        print(
+            "Invalid filepath: should end in .txt extension;"
+            " file must be a text file."
+        )
+        return np.zeros((9, 9))
+    else:
+        try:
+            with open(filepath, "r") as file:
+                file_lines = file.readlines()
+        except FileNotFoundError:
+            print("Invalid filepath: text file does not exist.")
+            return np.zeros((9, 9))
+        else:
+            file_digits = []
+            for line in file_lines:
+                line_digits = [int(char) for char in line if char.isdigit()]
+                if line_digits:
+                    file_digits.append(line_digits)
+            if len(file_digits) != 9:
+                print(
+                    "Invalid text file: must have exactly 9 lines that "
+                    "contain digits."
+                )
+                return np.zeros((9, 9))
+            elif [len(digits_list) for digits_list in file_digits] != [9] * 9:
+                print(
+                    "Invalid text file: must have exactly 9 digits per "
+                    "line."
+                )
+                return np.zeros((9, 9))
+            else:
+                grid = np.array(file_digits)
+                return grid
 
 
 def write_grid(grid, filepath=None):
