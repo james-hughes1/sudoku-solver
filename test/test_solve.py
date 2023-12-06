a128b6e39b63e097cb9f47f51158518bb6586b13
James Hughes <jh2284@cam.ac.uk>
2023-12-06 09:11:36 +0000

    Test: Split up solve tests, added solve_backtrack error catch tests

--- test/test_solve.py
+++ test/test_solve.py
@@ -21,19 +21,6 @@ start_grid_easy = np.array(
     ]
 )
 
-start_grid_medium = np.array(
-    [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [4, 5, 6, 7, 0, 9, 1, 2, 3],
-        [0, 8, 9, 1, 2, 3, 4, 5, 6],
-        [2, 3, 4, 5, 0, 7, 8, 9, 1],
-        [0, 6, 7, 8, 9, 1, 2, 3, 4],
-        [8, 9, 1, 2, 3, 0, 5, 6, 7],
-        [0, 4, 5, 6, 7, 8, 9, 1, 2],
-        [6, 7, 8, 9, 1, 0, 3, 4, 5],
-        [9, 0, 2, 3, 4, 5, 6, 7, 8],
-    ]
-)
 
 solution_grid_expected_1 = np.array(
     [
@@ -46,65 +33,6 @@ solution_grid_expected_1 = np.array(
         [3, 4, 5, 6, 7, 8, 9, 1, 2],
         [6, 7, 8, 9, 1, 2, 3, 4, 5],
         [9, 1, 2, 3, 4, 5, 6, 7, 8],
-    ]
-)
-
-# Example from
-# https://en.wikipedia.org/wiki/Mathematics_of_Sudoku#Sudokus_with_Few_Clues
-start_grid_minimal = np.array(
-    [
-        [0, 0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 2, 0, 0, 3],
-        [0, 0, 0, 4, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 5, 0, 0],
-        [4, 0, 1, 6, 0, 0, 0, 0, 0],
-        [0, 0, 7, 1, 0, 0, 0, 0, 0],
-        [0, 5, 0, 0, 0, 0, 2, 0, 0],
-        [0, 0, 0, 0, 8, 0, 0, 4, 0],
-        [0, 3, 0, 9, 1, 0, 0, 0, 0],
-    ]
-)
-
-solution_grid_expected_2 = np.array(
-    [
-        [7, 4, 5, 3, 6, 8, 9, 1, 2],
-        [8, 1, 9, 5, 7, 2, 4, 6, 3],
-        [3, 6, 2, 4, 9, 1, 8, 5, 7],
-        [6, 9, 3, 8, 2, 4, 5, 7, 1],
-        [4, 2, 1, 6, 5, 7, 3, 9, 8],
-        [5, 8, 7, 1, 3, 9, 6, 2, 4],
-        [1, 5, 8, 7, 4, 6, 2, 3, 9],
-        [9, 7, 6, 2, 8, 3, 1, 4, 5],
-        [2, 3, 4, 9, 1, 5, 7, 8, 6],
-    ]
-)
-
-# Example from https://en.wikipedia.org/wiki/Sudoku_solving_algorithms
-start_grid_antagonist = np.array(
-    [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 3, 0, 8, 5],
-        [0, 0, 1, 0, 2, 0, 0, 0, 0],
-        [0, 0, 0, 5, 0, 7, 0, 0, 0],
-        [0, 0, 4, 0, 0, 0, 1, 0, 0],
-        [0, 9, 0, 0, 0, 0, 0, 0, 0],
-        [5, 0, 0, 0, 0, 0, 0, 7, 3],
-        [0, 0, 2, 0, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 4, 0, 0, 0, 9],
-    ]
-)
-
-solution_grid_expected_3 = np.array(
-    [
-        [9, 8, 7, 6, 5, 4, 3, 2, 1],
-        [2, 4, 6, 1, 7, 3, 9, 8, 5],
-        [3, 5, 1, 9, 2, 8, 7, 4, 6],
-        [1, 2, 8, 5, 3, 7, 6, 9, 4],
-        [6, 3, 4, 8, 9, 2, 1, 5, 7],
-        [7, 9, 5, 4, 6, 1, 8, 3, 2],
-        [5, 1, 9, 2, 8, 6, 4, 7, 3],
-        [4, 7, 2, 3, 1, 9, 5, 6, 8],
-        [8, 6, 3, 7, 4, 5, 2, 1, 9],
     ]
 )
 
@@ -128,16 +56,43 @@ def test_solve_backtrack_easy():
     assert (solution_grid == solution_grid_expected_1).all()
 
 
-def test_solve_backtrack_medium():
-    solution_grid = solve_backtrack(start_grid_medium)
-    assert (solution_grid == solution_grid_expected_1).all()
+def test_solve_backtrack_invalid(capfd):
+    start_grid_invalid = start_grid_easy.copy()
+    start_grid_invalid[0, 0] = 2
+    solve_backtrack(start_grid_invalid)
+    captured = capfd.readouterr()
+    assert (
+        captured.out == "Invalid starting grid, each digit can only appear "
+        "once in each row, column and 3x3 box."
+    )
 
 
-def test_solve_backtrack_minimal():
-    solution_grid = solve_backtrack(start_grid_minimal)
-    assert (solution_grid == solution_grid_expected_2).all()
-
-
-def test_solve_backtrack_antagonist():
-    solution_grid = solve_backtrack(start_grid_antagonist)
-    assert (solution_grid == solution_grid_expected_3).all()
+def test_solve_backtrack_few_clues(capfd):
+    start_grid = np.zeros((9, 9))
+    start_grid[0, 0] = 1
+    solution_grid = solve_backtrack(start_grid)
+    # Check that the solution is valid
+    solve = True
+    for element_idx in range(9):
+        _, counts = np.unique(
+            solution_grid[element_idx, :], return_counts=True
+        )
+        if (counts[1:] > 1).any():
+            solve = False
+        _, counts = np.unique(
+            solution_grid[:, element_idx], return_counts=True
+        )
+        if (counts[1:] > 1).any():
+            solve = False
+        _, counts = np.unique(
+            solution_grid[
+                3 * (element_idx // 3) : 3 * (element_idx // 3) + 3,
+                3 * (element_idx % 3) : 3 * (element_idx % 3) + 3,
+            ],
+            return_counts=True,
+        )
+        if (counts[1:] > 1).any():
+            solve = False
+    if (solution_grid == 0).any():
+        solve = False
+    assert solve
---
+++ test/test_solve_difficult.py
@@ -0,0 +1,104 @@
+import numpy as np
+from src.sudokutools.solve import solve_backtrack
+
+start_grid_medium = np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 5, 6, 7, 0, 9, 1, 2, 3],
+        [0, 8, 9, 1, 2, 3, 4, 5, 6],
+        [2, 3, 4, 5, 0, 7, 8, 9, 1],
+        [0, 6, 7, 8, 9, 1, 2, 3, 4],
+        [8, 9, 1, 2, 3, 0, 5, 6, 7],
+        [0, 4, 5, 6, 7, 8, 9, 1, 2],
+        [6, 7, 8, 9, 1, 0, 3, 4, 5],
+        [9, 0, 2, 3, 4, 5, 6, 7, 8],
+    ]
+)
+
+solution_grid_expected_1 = np.array(
+    [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [4, 5, 6, 7, 8, 9, 1, 2, 3],
+        [7, 8, 9, 1, 2, 3, 4, 5, 6],
+        [2, 3, 4, 5, 6, 7, 8, 9, 1],
+        [5, 6, 7, 8, 9, 1, 2, 3, 4],
+        [8, 9, 1, 2, 3, 4, 5, 6, 7],
+        [3, 4, 5, 6, 7, 8, 9, 1, 2],
+        [6, 7, 8, 9, 1, 2, 3, 4, 5],
+        [9, 1, 2, 3, 4, 5, 6, 7, 8],
+    ]
+)
+
+# Example from
+# https://en.wikipedia.org/wiki/Mathematics_of_Sudoku#Sudokus_with_Few_Clues
+start_grid_minimal = np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 2, 0, 0, 3],
+        [0, 0, 0, 4, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 5, 0, 0],
+        [4, 0, 1, 6, 0, 0, 0, 0, 0],
+        [0, 0, 7, 1, 0, 0, 0, 0, 0],
+        [0, 5, 0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 0, 0, 8, 0, 0, 4, 0],
+        [0, 3, 0, 9, 1, 0, 0, 0, 0],
+    ]
+)
+
+solution_grid_expected_2 = np.array(
+    [
+        [7, 4, 5, 3, 6, 8, 9, 1, 2],
+        [8, 1, 9, 5, 7, 2, 4, 6, 3],
+        [3, 6, 2, 4, 9, 1, 8, 5, 7],
+        [6, 9, 3, 8, 2, 4, 5, 7, 1],
+        [4, 2, 1, 6, 5, 7, 3, 9, 8],
+        [5, 8, 7, 1, 3, 9, 6, 2, 4],
+        [1, 5, 8, 7, 4, 6, 2, 3, 9],
+        [9, 7, 6, 2, 8, 3, 1, 4, 5],
+        [2, 3, 4, 9, 1, 5, 7, 8, 6],
+    ]
+)
+
+# Example from https://en.wikipedia.org/wiki/Sudoku_solving_algorithms
+start_grid_antagonist = np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 3, 0, 8, 5],
+        [0, 0, 1, 0, 2, 0, 0, 0, 0],
+        [0, 0, 0, 5, 0, 7, 0, 0, 0],
+        [0, 0, 4, 0, 0, 0, 1, 0, 0],
+        [0, 9, 0, 0, 0, 0, 0, 0, 0],
+        [5, 0, 0, 0, 0, 0, 0, 7, 3],
+        [0, 0, 2, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 4, 0, 0, 0, 9],
+    ]
+)
+
+solution_grid_expected_3 = np.array(
+    [
+        [9, 8, 7, 6, 5, 4, 3, 2, 1],
+        [2, 4, 6, 1, 7, 3, 9, 8, 5],
+        [3, 5, 1, 9, 2, 8, 7, 4, 6],
+        [1, 2, 8, 5, 3, 7, 6, 9, 4],
+        [6, 3, 4, 8, 9, 2, 1, 5, 7],
+        [7, 9, 5, 4, 6, 1, 8, 3, 2],
+        [5, 1, 9, 2, 8, 6, 4, 7, 3],
+        [4, 7, 2, 3, 1, 9, 5, 6, 8],
+        [8, 6, 3, 7, 4, 5, 2, 1, 9],
+    ]
+)
+
+
+def test_solve_backtrack_medium():
+    solution_grid = solve_backtrack(start_grid_medium)
+    assert (solution_grid == solution_grid_expected_1).all()
+
+
+def test_solve_backtrack_minimal():
+    solution_grid = solve_backtrack(start_grid_minimal)
+    assert (solution_grid == solution_grid_expected_2).all()
+
+
+def test_solve_backtrack_antagonist():
+    solution_grid = solve_backtrack(start_grid_antagonist)
+    assert (solution_grid == solution_grid_expected_3).all()
